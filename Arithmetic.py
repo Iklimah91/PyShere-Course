f5be76b86164962e42c8c23145038d99a13a96bb
Iklimah91 <125455655+Iklimah91@users.noreply.github.com>
2025-08-10 01:02:21 +0200

    Added solution for Ohm's Law assignment

---
+++ Arithmetic.py
@@ -0,0 +1,15 @@
+for i in range(5):
+        print ("Loop:", i)
+
+        count = 0
+        while count < 5:
+            print ("count :", count)
+            count += 1
+
+            num = 10
+if num > 0:
+    print ( "Greater than zero")
+elif num == 10:
+    print ( "Equal to 10")
+else: 
+    print ( "Less than zero")
---
+++ Practical Example 1.py
@@ -0,0 +1,27 @@
+print ( "welcome to python")
+
+name = input( "Enter your name; ")
+print ( "Hello", name)
+Age = input( "Enter your age; ")
+print ( "You are", Age, "years old")
+print ( "Thank you for using this program!")
+print ( "Have a great day" )
+
+
+
+x = 5
+y = 10
+print ( x + y )
+print ( x < y or x == y )
+print ( not (x < y) )
+
+
+num = 10
+if num > 0:
+    print ( "Greater than zero")
+elif num == 10:
+    print ( "Equal to 10")
+else: 
+    print ( "Less than zero")
+
+    
