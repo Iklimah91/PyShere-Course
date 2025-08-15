7b362b9221ba5a5c72ee5943424bd57971825612
Iklimah91 <125455655+Iklimah91@users.noreply.github.com>
2025-08-15 11:02:16 +0200

    Variables & Datatypes - Exercise

---
+++ PE 4-Scrimba.py
@@ -0,0 +1,12 @@
+#Create appropriate Variables for Item name, the price 
+#and how many you have in stock
+
+item_name = input("Enter item name: ")
+price = input('Enter the price: ')
+stock = input('How many do you have in stock? ')
+print("item name:", item_name, "price:", price, "- (stock:", stock, ")")
+
+item_name = "tomatoes"
+price = 20
+inevtory = 100
+print ("item_name", "price" - "inventory")
